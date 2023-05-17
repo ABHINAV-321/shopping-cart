66ef161b91144c2a874de3484c30e765e2e37319
ABHINAV TS <abhinavts444@gmail.com>
2023-05-17 09:03:40 +0530

    added admin add product && and working perfectly on md devices

--- helpers/Product-helpers.js
+++ helpers/Product-helpers.js
@@ -24,5 +24,17 @@ module.exports={
    //  console.log(data.insertedId.toString() )
       callback(data.insertedId.toString()) 
     }) 
+  }, 
+  getAllProducts:()=>{
+    return new Promise(async(resolve, reject)=>{
+      let product=await client.db('shopping-cart').collection('product').find().toArray()
+/*let productsId=await client.db('shopping-cart').collection('product').find({},{"_id":0}).toArray()*/
+console.log(product.name)
+let id =product._id 
+
+console.log("id jhggg"+id+" profuct id here")
+    resolve(product)
+      
+    })
   }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,37 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var productHelper =require('../helpers/Product-helpers')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let products=[
-    {
-    name:"iphone 14 pro max ", 
-    img:"https://media.croma.com/image/upload/v1662655485/Croma%20Assets/Communication/Mobiles/Images/261971_c8p8eb.png" , 
-    des:"latest iphone model 14 pro max with 128gb sd", 
-    price:140000
-  }, 
-  {
-    name:"i watch series 8", 
-    des:"with huge display, gps, Bluetooth,.....", 
-    price:89000, 
-    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc_46L5h4D_3YjyDbVILMMryJQGzv27E2V8iUU520TyA&usqp=CAU&ec=48600113"
-  
-    
-  }, 
-  {
-    name:"Apple ear pods pro", 
-    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSwPCbKzlYNa5QISiL5EKb8zamYIGoqfagdPAznG24QQ&usqp=CAU&ec=48600113", 
-    des:"brand Apple, ear pods with Bluetooth", 
-    price:25000
-  }, 
-  {
-    name:"apple mac book 2023 512 GB", 
-    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8qrvXnOeX9dUXQX6xnSODGD0-Km15tsjwSygI_BgWaQ&usqp=CAU&ec=48600113",
-    des:"mac book with 512 gb varient ", 
-    price:"249,000"
-  }
-  ]
-  res.render('index', { products,admin:false});
+productHelper.getAllProducts().then((product)=>{
+
+  res.render('index', { product,admin:false});
+}); 
 });
 
 module.exports = router;
